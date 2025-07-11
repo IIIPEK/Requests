45ca44e19ee26383011bf8a3456faf40e3fcffc9
Ivan Vorobev <ivan.vorobjov@gmail.com>
2025-07-11 10:02:35 +0300

    Corrected folder name and settings

--- Request_Project/urls.py
+++ Request_Project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Reqest_Project project.
+URL configuration for Request_Project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
